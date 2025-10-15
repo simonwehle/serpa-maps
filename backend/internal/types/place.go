d10b65a51a898be5951507cdb420498727cd1dd0
simonwehle <simon.wehle@gmx.net>
2025-10-15 20:07:01 +0200

    fix(be): always include description

--- backend/internal/types/place.go
+++ backend/internal/types/place.go
@@ -8,7 +8,7 @@ type Place struct {
 	PlaceID     int       `db:"place_id" json:"place_id"`
 	//UserID      int     `db:"user_id" json:"user_id"`
 	Name        string    `db:"name" json:"name"`
-	Description string    `db:"description" json:"description,omitempty"`
+	Description string    `db:"description" json:"description"`
 	Latitude    float64   `db:"latitude" json:"latitude"`
 	Longitude   float64   `db:"longitude" json:"longitude"`
 	CategoryID  int       `db:"category_id" json:"category_id"`
