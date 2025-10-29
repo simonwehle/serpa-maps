e988a994702d8945d5270360550efce3c984dd69
simonwehle <simon.wehle@gmx.net>
2025-10-29 21:40:00 +0100

    WIP: Vector Tiles

--- frontend/lib/pages/flutter_map.dart
+++ frontend/lib/pages/flutter_map.dart
@@ -3,9 +3,33 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:serpa_maps/widgets/markers/place_marker.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:vector_map_tiles/vector_map_tiles.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class FlutterMapScreen extends StatelessWidget {
+class FlutterMapScreen extends StatefulWidget {
   const FlutterMapScreen({super.key});
+  @override
+  State<FlutterMapScreen> createState() => _FlutterMapScreenState();
+}
+
+class _FlutterMapScreenState extends State<FlutterMapScreen> {
+  Style? style;
+
+  @override
+  void initState() {
+    super.initState();
+
+    StyleReader(
+      uri: 'https://api.maptiler.com/maps/streets/style.json?key={key}',
+      // ignore: undefined_identifier
+      apiKey: dotenv.env['API_KEY'],
+      //logger: const Logger.console(),
+    ).read().then((style) {
+      this.style = style;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -18,12 +42,17 @@ class FlutterMapScreen extends StatelessWidget {
         ),
       ),
       children: [
-        TileLayer(
-          // Bring your own tiles
-          urlTemplate:
-              'https://a.tile.openstreetmap.de/{z}/{x}/{y}.png', // For demonstration only
-          userAgentPackageName: 'com.serpamaps.app', // Add your app identifier
-          // And many more recommended properties!
+        // TileLayer(
+        //   // Bring your own tiles
+        //   urlTemplate:
+        //       'https://a.tile.openstreetmap.de/{z}/{x}/{y}.png', // For demonstration only
+        //   userAgentPackageName: 'com.serpamaps.app', // Add your app identifier
+        //   // And many more recommended properties!
+        // ),
+        VectorTileLayer(
+          tileProviders: style!.providers,
+          theme: style!.theme,
+          tileOffset: TileOffset.DEFAULT,
         ),
         MarkerLayer(
           markers: [
