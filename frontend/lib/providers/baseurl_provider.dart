6cccc5d7522f788b01bd1fe11c61a9b44db54ff3
simonwehle <simon.wehle@gmx.net>
2025-10-29 17:11:30 +0100

    fix(fe): cleanup

--- frontend/lib/providers/baseurl_provider.dart
+++ frontend/lib/providers/baseurl_provider.dart
@@ -1,13 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class BaseUrlNotifier extends Notifier<String> {
-  @override
-  String build() {
-    return dotenv.env['BASE_URL'] ?? "http://localhost:3465";
-  }
-}
-
-final baseUrlProvider = NotifierProvider<BaseUrlNotifier, String>(
-  BaseUrlNotifier.new,
-);
+final baseUrlProvider = Provider<String>((ref) {
+  return dotenv.env['BASE_URL'] ?? "http://localhost:3465";
+});
