12c154846e053009b2b262adee8c743e0942d17d
simonwehle <simon.wehle@gmx.net>
2025-10-30 12:54:01 +0100

    feat(fe): add compass to map

--- frontend/lib/pages/map_screen.dart
+++ frontend/lib/pages/map_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_compass/flutter_map_compass.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -32,37 +33,48 @@ class _MapScreenState extends ConsumerState<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(0, 0),
-        initialZoom: 2,
-        // Orientation Lock
-        interactionOptions: const InteractionOptions(
-          flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(0, 0),
+          initialZoom: 2,
+          // Orientation Lock
+          // interactionOptions: const InteractionOptions(
+          //   flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+          // ),
         ),
+        children: [
+          TileLayer(
+            // Bring your own tiles
+            urlTemplate:
+                'https://a.tile.openstreetmap.de/{z}/{x}/{y}.png', // For demonstration only
+            userAgentPackageName:
+                'com.serpamaps.app', // Add your app identifier
+            // And many more recommended properties!
+          ),
+          const MapCompass.cupertino(
+            hideIfRotatedNorth: true,
+            padding: EdgeInsets.fromLTRB(0, 40, 10, 0),
+          ),
+          MarkerLayer(markers: markerList),
+          RichAttributionWidget(
+            // Include a stylish prebuilt attribution widget that meets all requirments
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+                onTap: () => launchUrl(
+                  Uri.parse('https://openstreetmap.org/copyright'),
+                ), // (external)
+              ),
+              // Also add images...
+            ],
+          ),
+        ],
       ),
-      children: [
-        TileLayer(
-          // Bring your own tiles
-          urlTemplate:
-              'https://a.tile.openstreetmap.de/{z}/{x}/{y}.png', // For demonstration only
-          userAgentPackageName: 'com.serpamaps.app', // Add your app identifier
-          // And many more recommended properties!
-        ),
-        MarkerLayer(markers: markerList),
-        RichAttributionWidget(
-          // Include a stylish prebuilt attribution widget that meets all requirments
-          attributions: [
-            TextSourceAttribution(
-              'OpenStreetMap contributors',
-              onTap: () => launchUrl(
-                Uri.parse('https://openstreetmap.org/copyright'),
-              ), // (external)
-            ),
-            // Also add images...
-          ],
-        ),
-      ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("pressed"),
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
