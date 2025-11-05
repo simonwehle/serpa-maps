e14cd197a5f20a5fbb3fd1acded367df31b856c7
simonwehle <simon.wehle@gmx.net>
2025-11-05 23:29:31 +0100

    fix(fe): update baseUrlProvider

--- frontend/lib/providers/api_provider.dart
+++ frontend/lib/providers/api_provider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:serpa_maps/providers/baseurl_provider.dart';
 import 'package:serpa_maps/services/api_service.dart';
 
 final apiServiceProvider = Provider<ApiService>((ref) {
-  final baseUrl = dotenv.env['BASE_URL'] ?? "http://localhost:3465";
+  final baseUrl = ref.read(baseUrlProvider);
   return ApiService(baseUrl, apiVersion: '/api/v1');
 });
--- frontend/lib/providers/baseurl_provider.dart
+++ frontend/lib/providers/baseurl_provider.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final baseUrlProvider = Provider<String>((ref) {
-  return dotenv.env['BASE_URL'] ?? "http://localhost:3465";
+  return dotenv.env['BASE_URL'] ?? "http://localhost:53164";
 });
