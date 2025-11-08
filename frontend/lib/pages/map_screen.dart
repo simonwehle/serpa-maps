f004d220c403859338de032cd24544cb295d7116
simonwehle <simon.wehle@gmx.net>
2025-11-08 15:49:15 +0100

    fix(fe): cleanup 2

--- frontend/lib/pages/map_screen.dart
+++ frontend/lib/pages/map_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_compass/flutter_map_compass.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
