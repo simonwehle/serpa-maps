27c2b1021a0317da118d2199748a64a8a949db5c
simonwehle <simon.wehle@gmx.net>
2025-11-20 11:33:05 +0100

    fix: add text inside the image

--- frontend/lib/widgets/layer/layer_image.dart
+++ frontend/lib/widgets/layer/layer_image.dart
@@ -17,20 +17,43 @@ class LayerImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(radius!),
-            border: Border.all(color: Colors.grey),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(radius!),
-            child: Image(image: assetImage, width: width, height: height),
+    return Padding(
+      padding: EdgeInsetsGeometry.all(8),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius!),
+          border: Border.all(color: Colors.grey),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(radius!),
+          child: Stack(
+            children: [
+              Image(image: assetImage, width: width, height: height),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 12,
+                  ),
+                  color: Colors.black.withValues(alpha: 0.4),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        Text(name),
-      ],
+      ),
     );
   }
 }
