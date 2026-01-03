a68983f84a9ff8f3c2235a6e2e1c19bdd364ef76
simonwehle <simon.wehle@gmx.net>
2026-01-03 17:02:31 +0100

    feat(fe): add welcome screen

---
+++ frontend/lib/pages/welcome_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+//import 'package:serpa_maps/l10n/app_localizations.dart';
+//import 'package:serpa_maps/providers/base_url_provider.dart';
+import 'package:serpa_maps/providers/style_dark_provider.dart';
+import 'package:serpa_maps/providers/style_provider.dart';
+
+class WelcomeScreen extends ConsumerStatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  ConsumerState<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
+  bool moreOptions = false;
+  late TextEditingController baseUrlController;
+  late TextEditingController styleUrlController;
+  late TextEditingController styleDarkUrlController;
+
+  @override
+  void initState() {
+    super.initState();
+    baseUrlController = TextEditingController();
+    styleUrlController = TextEditingController();
+    styleDarkUrlController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    baseUrlController.dispose();
+    styleUrlController.dispose();
+    styleDarkUrlController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //final i10n = AppLocalizations.of(context)!;
+    final styleUrl = ref.read(styleUrlProvider);
+    final styleDarkUrl = ref.read(styleDarkUrlProvider);
+    if (styleUrlController.text.isEmpty && styleUrl.isNotEmpty) {
+      styleUrlController.text = styleUrl;
+    }
+    if (styleDarkUrlController.text.isEmpty && styleDarkUrl.isNotEmpty) {
+      styleDarkUrlController.text = styleDarkUrl;
+    }
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome to Serpa Maps"),
+            const SizedBox(height: 24),
+            TextField(
+              controller: baseUrlController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Base URL',
+              ),
+            ),
+            const SizedBox(height: 8),
+            if (moreOptions) ...[
+              TextField(
+                controller: styleUrlController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Style URL',
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: styleDarkUrlController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Dark Style URL',
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Overlay URL (optional)',
+                ),
+              ),
+              const SizedBox(height: 8),
+            ],
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    moreOptions ? "less options" : "more options",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  moreOptions = !moreOptions;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
