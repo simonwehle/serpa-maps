ac763f719c74cac2c8687d6c00a5ffc79c5c2bae
simonwehle <simon.wehle@gmx.net>
2026-01-08 21:44:35 +0100

    fix(fe): add description and remove alpha channel

--- frontend/lib/utils/icon_color_utils.dart
+++ frontend/lib/utils/icon_color_utils.dart
@@ -9,7 +9,7 @@ String _channelToHex(num channel) => ((channel * 255.0).round() & 0xff)
 
 String colorToHex(Color color) {
   return '#'
-      '${_channelToHex(color.a)}'
+      //'${_channelToHex(color.a)}'
       '${_channelToHex(color.r)}'
       '${_channelToHex(color.g)}'
       '${_channelToHex(color.b)}';
