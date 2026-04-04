5146d2743e04bd10f1c63bd68fef3efdecb9c80e
simonwehle <simon.wehle@gmx.net>
2026-04-04 23:36:28 +0200

    fix(fe): invalidate categories and places after accepting invite

--- frontend/lib/providers/data/invite_provider.dart
+++ frontend/lib/providers/data/invite_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:serpa_maps/models/invite.dart';
 import 'package:serpa_maps/providers/api/api_provider.dart';
+import 'package:serpa_maps/providers/data/category_provider.dart';
 import 'package:serpa_maps/providers/data/group_provider.dart';
+import 'package:serpa_maps/providers/data/place_provider.dart';
 
 final inviteProvider = AsyncNotifierProvider<InviteNotifier, List<Invite>>(
   InviteNotifier.new,
@@ -23,6 +25,10 @@ class InviteNotifier extends AsyncNotifier<List<Invite>> {
     state = state.whenData(
       (invites) => invites.where((i) => i.groupInviteId != id).toList(),
     );
-    ref.invalidate(groupProvider);
+    if (accept) {
+      ref.invalidate(groupProvider);
+      ref.invalidate(categoryProvider);
+      ref.invalidate(placeProvider);
+    }
   }
 }
